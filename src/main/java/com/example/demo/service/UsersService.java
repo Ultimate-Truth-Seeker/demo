fae6207414705c1182936cae5d33397bc8dab5ae
Ultimate-Truth-Seeker <96703353+Ultimate-Truth-Seeker@users.noreply.github.com>
2024-05-26 23:16:44 -0600

    primera version microservicios

---
+++ src/main/java/com/example/demo/service/UsersService.java
@@ -0,0 +1,20 @@
+package com.example.demo.service;
+
+import com.example.demo.repository.User;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface UsersService {
+
+    User save(User user);
+
+
+    Optional<User> findById(String id);
+
+    List<User> all();
+
+    void deleteById(String id);
+
+    User update(User user, String userId);
+}
